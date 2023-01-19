f0023ffd656909d2c75887b7e2cef1cedb1d6f69
asnganje <abd.nganje@gmail.com>
2023-01-19 14:49:27 +0300

    modify foreign key

--- db/migrate/20230118205940_create_posts.rb
+++ db/migrate/20230118205940_create_posts.rb
@@ -2,10 +2,10 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :text
-      t.string :comments_counter
-      t.string :likes_counter
-      t.references :user, null: false, foreign_key: true
+      t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.references :author, null: false, foreign_key: { to_table: :users }
       
       t.timestamps
     end
--- db/migrate/20230118205951_create_likes.rb
+++ db/migrate/20230118205951_create_likes.rb
@@ -1,9 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
-
+      t.references :post, foreign_key: { to_table: :posts}
+      t.references :author, foreign_key: { to_table: :users }
+      
       t.timestamps
     end
   end
