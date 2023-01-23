8fbc9f669eaa1f098ab9c78bc51412b6063ef5b0
asnganje <abd.nganje@gmail.com>
2023-01-23 17:33:47 +0300

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   # GET /posts or /posts.json
-  def index;
-  end
+  def index; end
 
   # GET /posts/1 or /posts/1.json
   def show; end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
   # GET /users or /users.json
-  def index;
-  end
+  def index; end
 
   # GET /users/1 or /users/1.json
   def show; end
-
 end
