dc526f56505485a0e2694fcf5f1c6481d4a22879
asnganje <abd.nganje@gmail.com>
2023-01-24 13:30:04 +0300

    modfy posts and users controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   # GET /posts or /posts.json
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+  end
 
   # GET /posts/1 or /posts/1.json
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   # GET /users or /users.json
-  def index; end
+  def index
+    @users = User.all
+  end
 
   # GET /users/1 or /users/1.json
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
