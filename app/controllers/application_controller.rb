cd440792d0a3b01170c5cf9ba9a68e526b26a58f
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-02 00:44:27 +0300

    Add filter for after signing out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 
   protected
 
