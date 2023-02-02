19b57500c4872e116a94504dcf2ba143364199c7
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-02 17:55:01 +0300

    Add method for removing a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,8 +25,14 @@ class PostsController < ApplicationController
     end
   end
   
+  def destroy
+    @post = Post.find(params[:id])
+    @post.author.decrement!(:posts_counter)
+    @post.destroy
 
-  
+    redirect_to root_path, status: :see_other  
+  end
+    
   private
 
   def post_params
