aa369cc90660990f142fd1304088188392384c33
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-02 17:56:40 +0300

    Add user privilages for managing posts and comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,19 @@
 class Ability
   include CanCan::Ability
 
- 
+  def initialize(user)
+    can :read, Post
+
+    return unless user.present?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id 
+    
+    return unless user.role == 'admin'
+
+    can :manage, :all
+  end
+  
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
